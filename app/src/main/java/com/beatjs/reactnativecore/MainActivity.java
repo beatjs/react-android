6324e4185b6ddf328b4a15047292ba1de4e6c919
Liam <liamxujia@outlook.com>
2021-04-09 18:12:29 +0800

    [bundle] locate react native

--- app/src/main/java/com/beatjs/reactnativecore/MainActivity.java
+++ app/src/main/java/com/beatjs/reactnativecore/MainActivity.java
@@ -3,16 +3,43 @@ package com.beatjs.reactnativecore;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
-import com.beatjs.react.Tools;
+import com.facebook.react.ReactInstanceManager;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.ReactRootView;
+import com.facebook.react.common.LifecycleState;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
-//    private ReactRootView mReactRootView;
+    private ReactRootView mReactRootView;
+    private ReactInstanceManager mReactInstanceManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        System.out.println("===========react-native-core-android" + Tools.NUMBER);
+        SoLoader.init(this, false);
+
+        mReactRootView = new ReactRootView(this);
+        List<ReactPackage> packages = Arrays.asList(new MainReactPackage());
+
+        mReactInstanceManager = ReactInstanceManager.builder()
+                .setApplication(getApplication())
+                .setCurrentActivity(this)
+                .setBundleAssetName("index.jsbundle")
+                .setJSMainModulePath("index")
+                .addPackages(packages)
+                .setUseDeveloperSupport(BuildConfig.DEBUG)
+                .setInitialLifecycleState(LifecycleState.RESUMED)
+                .build();
+
+        mReactRootView.startReactApplication(mReactInstanceManager, "AwesomeProject", null);
+
+        setContentView(mReactRootView);
     }
 }
