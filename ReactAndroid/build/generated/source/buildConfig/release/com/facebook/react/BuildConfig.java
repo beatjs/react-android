72b3ec78b45d3343ea0acaa64f861f411b8794af
Liam <liamxujia@outlook.com>
2021-04-09 15:23:13 +0800

    [gradlew] installArchives

---
+++ ReactAndroid/build/generated/source/buildConfig/release/com/facebook/react/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.facebook.react;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = false;
+  public static final String LIBRARY_PACKAGE_NAME = "com.facebook.react";
+  public static final String BUILD_TYPE = "release";
+  // Field from default config.
+  public static final int EXOPACKAGE_FLAGS = 0;
+  // Field from default config.
+  public static final boolean IS_INTERNAL_BUILD = false;
+}
